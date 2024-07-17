aa665e433847cb9930d61a102e339d8c767152d9
LeoYang <yangst1258768646@gmail.com>
2024-07-17 12:31:24 -0400

    change

--- app/main_new.py
+++ app/main_new.py
@@ -22,10 +22,40 @@ base_ormar_config = ormar.OrmarConfig(
 class User(ormar.Model):
     ormar_config = base_ormar_config.copy(tablename="users")
 
-    id: int = ormar.Integer(primary_key=True) # type: ignore
-    user_name: str = ormar.String(min_length=3, max_length=12) # type: ignore
-    pwd: str = ormar.String(min_length=3, max_length=12) # type: ignore
+    id: int = ormar.Integer(primary_key=True)  # type: ignore
+    user_name: str = ormar.String(min_length=3, max_length=12)  # type: ignore
+    pwd: str = ormar.String(min_length=3, max_length=12)  # type: ignore
 
+
+class Todo(ormar.Model):
+    id: int = Field(defult=None, primary_key=True)
+    item: str = Field(index=True)
+    create_time: datetime = Field(defult_factory=datetime.now)
+    plan_time: datetime = Field(defult=None)
+    user_id: int = Field(default=None, foreign_key="user.id")
+    user: User = Relationship(back_populates='todo_list')
+
+
+class PaginateModel(ormar.Model, Gneric[T]):
+    page: int
+    per_page: int
+    total_items: int
+    items: Sequence[T]
+
+
+class UpdateTodoDto(ormar.Model):
+    item: str
+    plan_time: str
+
+    @field_validator('plan_time')
+    @classmethod
+    def parse_plan_time(cls, value: str):
+        if value:
+            return datetime.strptime(value, TIME_FORMAT)
+        return value
+
+
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
 async def init_db_and_tables():
@@ -52,14 +82,12 @@ async def init_db_and_tables():
         "Desk"
     ]
     init_todo_user_ids = [1, 2, 3, 4]
-    # with Session(engine) as session:
-    #     for i in range(len(init_todos)):
-    #         init_todo = init_todos[i]
-    #         init_todo_user_id = init_todo_user_ids[i % 4]
-    #         user = session.exec(select(User).where(User.id == init_todo_user_id)).first()
-    #         todo = Todo(item=init_todo, plan_time=datetime.now(), user_id=init_todo_user_id, user=user)  # type: ignore
-    #         session.add(todo)
-    #     session.commit()
+    for i in range(len(init_todos)):
+        init_todo = init_todos[i]
+        init_todo_user_id = init_todo_user_ids[i % 4]
+        user = session.exec(select(User).where(User.id == init_todo_user_id)).first()
+        todo = Todo(item=init_todo, plan_time=datetime.now(), user_id=init_todo_user_id, user=user)  # type: ignore
+        session.add(todo)
 
 
 @asynccontextmanager
